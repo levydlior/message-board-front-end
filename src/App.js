cf6c47f896510eaba1c604fbb5e7826caac43020
Lior <levydlior@gmail.com>
2022-07-06 13:02:48 -0600

    add fetch, edit some message style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import "./styles/app.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/message_board")
+      .then((r) => r.json())
+      .then((messages) => setMessages(messages));
+  }, []);
+console.log(messages)
+
+
+
+const displayMessages = messages.map(message => {
+  const timestamp = new Date(message.created_at).toLocaleTimeString();
+  return (
+    <li className="message-box">
+      <img className="message-profile-pic" src={message.avatar_url} alt="profile picture" />
+      <h4>{message.user_name}</h4>
+      <p>{message.content}</p>
+      <p>{timestamp}</p>
+    </li>
+  )
+})
+
   return (
     <div className="App">
       <header id="header">
@@ -21,18 +45,13 @@ function App() {
           <h2> Messages:</h2>
           <div id="messages-display">
             <ul>
-              <li>message</li>
-              <li>message</li>
-              <li>message</li>
-              <li>message</li>
-              <li>message</li>
-              <li>message</li>
+             {displayMessages}
             </ul>
           </div>
           <div id="add-message">
             <h3>Post a message!</h3>
             <form>
-              <input type="text" required id="new-meesage-input"/>
+              <input type="text" required id="new-meesage-input" />
               <input type="submit" required value="post" />
             </form>
           </div>
