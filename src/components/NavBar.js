e936ad77a6d67de16282370137b329e656fa612c
Lior <levydlior@gmail.com>
2022-07-07 10:00:46 -0600

    changed nav bar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar({ loggedIn }) {
-    console.log(loggedIn, "logged")
   const displayLink = loggedIn ? (
     <NavLink className="nav-links" exact to="/account">
       My Account
     </NavLink>
-  ) : (
-    <NavLink className="nav-links" exact to="/create-login">
-      Login/ Create an Account
-    </NavLink>
-  );
+  ) : null;
 
   return (
     <nav>
@@ -20,6 +15,15 @@ function NavBar({ loggedIn }) {
           Message Board
         </NavLink>
         {displayLink}
+        {!loggedIn ? (
+          <NavLink className="nav-links" exact to="/create-login">
+            Login/ Create an Account
+          </NavLink>
+        ) : (
+          <NavLink className="nav-links" exact to="/create-login">
+            To Logout
+          </NavLink>
+        )}
       </ul>
     </nav>
   );
