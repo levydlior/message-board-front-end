83972ee8f2d3ee01bd3a09902654b3326b421544
Lior <levydlior@gmail.com>
2022-07-10 12:17:43 -0600

    edit profile avatar edit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import LoginCreateAccount from "./components/LoginCreateAccount";
 import MyProfile from "./components/MyProfile";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
   const [loggedInUser, setLoggedInUser] = useState({
-    userName: "",
-    userId: "",
+    userName: "a",
+    userId: 10,
   });
 
   const history = useHistory();
